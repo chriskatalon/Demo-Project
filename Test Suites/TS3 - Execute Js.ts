4c6617baa90ca5721cdabe444e987ed001e0b791
chriskatalon <quan.trinh@katalon.com>
2022-08-24 21:58:49 +0700

    sdaf
    dfsg

---
+++ Test Suites/TS3 - Execute Js.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS3 - Execute Js</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>003b8097-fb63-4ed2-a343-842b78fc7443</testSuiteGuid>
+</TestSuiteEntity>
