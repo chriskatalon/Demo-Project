8c8fab650ab819da3af41b2f98c75eebff5dfe9c
chriskatalon <quan.trinh@katalon.com>
2022-08-31 00:34:16 +0700

    ádf

--- Test Suites/TS3 - Execute Js.ts
+++ Test Suites/TS3 - Execute Js.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>003b8097-fb63-4ed2-a343-842b78fc7443</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d2f8386-14cd-4afa-a91a-9c5a31504041</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Execute JS</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
